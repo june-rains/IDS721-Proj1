3338111d6f148f6a9129c397b84194cfb1515b2a
changhao li <june.rainbowing@gmail.com>
2023-01-30 15:16:42 -0500

    add routes

--- application.py
+++ application.py
@@ -1,6 +1,20 @@
 from flask import Flask
+from flask import request
+
 application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@application.route('/name', methods=['GET', 'POST'])
+def get_name():
+    if request.method == 'GET':
+        return 'name from GET'
+    else:
+        return 'name from POST'
+
+@application.route('/fans')
+def get_fans():
+    return '100000'
