ef6d53782145cd8106978a5549c6b6d142e5a6cd
changhao li <june.rainbowing@gmail.com>
2023-02-05 21:31:52 -0500

    add front-end button and back-end post api

--- application.py
+++ application.py
@@ -8,57 +8,40 @@ application = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
-
-@application.route('/name', methods=['GET', 'POST'])
-def get_name():
-    if request.method == 'GET':
-        return 'name from GET'
-    else:
-        return 'name from POST'
-
-
-@application.route('/userProfile')
-def get_profile():
-    name = request.args.get('name', '')
-    if name == 'changhao':
-        return dict(name='changhao from GET', fans ='10000')
-    else:
-        return dict(name='nochanghao from GET', fans = '1000')
-
-
 # use this GET API to calculate the length of the longest palindrome substring
 # we assume that the input is just containing the digits and English letters
-@application.route('/lengthPalindrome')
+@application.route('/lengthPalindrome', methods=['POST'])
 def get_length():
-    input = request.args.get('input', '')
-    n = len(input)
-    # corner case: when the input is just one letter, we can directly return input
-    if n < 2:
-        return dict(longestPalindromeSubstring=input)
+    if request.method == 'POST':
+        input = request.form['input']
+        n = len(input)
+        # corner case: when the input is just one letter, we can directly return input
+        if n < 2:
+            return render_template('index.html', results=input)
 
-    # initialize the dp table
-    dp = [[False] * n for _ in range(n)]
-    # base case: single letter must be palindrome
-    for i in range(n):
-        dp[i][i] = True
-    
-    maxLen = 1
-    left = 0
-    right = 0
-    for i in range(n-1, -1, -1):
-        for j in range(i+1, n):
-            if j - i <= 2:
-                if input[i] == input[j]:
-                    dp[i][j] = True
-            else:
-                if input[i] == input[j]:
-                    dp[i][j] = dp[i+1][j-1]
-            
-            if dp[i][j] and j - i + 1 > maxLen:
-                maxLen = j - i + 1
-                left = i
-                right = j
-    return dict(longestPalindromeSubstring=input[left:right+1])
+        # initialize the dp table
+        dp = [[False] * n for _ in range(n)]
+        # base case: single letter must be palindrome
+        for i in range(n):
+            dp[i][i] = True
+        
+        maxLen = 1
+        left = 0
+        right = 0
+        for i in range(n-1, -1, -1):
+            for j in range(i+1, n):
+                if j - i <= 2:
+                    if input[i] == input[j]:
+                        dp[i][j] = True
+                else:
+                    if input[i] == input[j]:
+                        dp[i][j] = dp[i+1][j-1]
+                
+                if dp[i][j] and j - i + 1 > maxLen:
+                    maxLen = j - i + 1
+                    left = i
+                    right = j
+    return render_template('index.html', results=input[left:right+1])
 
 
 
